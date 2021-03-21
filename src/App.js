c517e44132c004be19bc7c52b317e78f77481469
Alvaro <alvinweeb@gmail.com>
2021-03-21 19:19:37 -0300

    refatoração

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { Notifyer } from './Notifyer.js';
 import { Timer } from './Timer.js';
 import { Emitter } from './Emitter.js'
 
+const notify = Notifyer.notify({
+    title: "Hora de postar!!",
+    body: "Corpo da mensagem."
+})
+
 const App = {
     async start() {
         try {
+            await Notifyer.init()
 
-            Emitter.on
+            Emitter.on('countdown-start', notify)
+            Emitter.on('countdown-end', Timer.init)
 
-            Timer.init(0.1 * 60)
-            // await Notifyer.init()
-            // Notifyer.notify({
-            //     title: "Hora de postar!!",
-            //     body: "Corpo da mensagem."
-            // })
+            Timer.init(20 * 60)
 
         } catch (err) {
             console.log(err.message);
